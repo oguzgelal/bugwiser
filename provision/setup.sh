c8c8d11fd4e8b41e96f23c7a1b9f723cd7222852
oguzgelal <o.gelal77@gmail.com>
2017-03-30 17:19:34 +0200

    reinstall wordpress

--- provision/setup.sh
+++ provision/setup.sh
@@ -10,7 +10,7 @@ sudo service php5-fpm restart
 
 # TODO - files changes
 
-#sudo apt-get install mariadb-server mariadb-client -y
+# sudo apt-get install mariadb-server mariadb-client -y
 # set MariaDB password - toor
 # MySQL create data base, create user
 # mysql -u root -p -e 'create database bugwiser; CREATE USER 'bugwiser'@'localhost' IDENTIFIED BY 'bugwiser';GRANT ALL PRIVILEGES ON bugwiser.* TO 'bugwiser'@'%' WITH GRANT OPTION;flush privileges;'
